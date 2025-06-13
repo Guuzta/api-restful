dbd257a0f33dd9391b1af64722bf3faadd5c653b
Guuzta <Guuhsilva13@outlook.com>
2025-06-13 16:48:04 -0300

    modificando endereco de conexao com o banco de dados

--- src/database/dataBase.js
+++ src/database/dataBase.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function connect () {
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/api-restful')
+        await mongoose.connect('mongodb://localhost:27017/api-restful')
         console.log('Conectado ao banco de dados!')
     } catch (error) {
         console.log('Erro ao conectar ao banco de dados!', error)
